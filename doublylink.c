69bd11869c8fe5a51f6c81bdf5edf72ae7a02a5d
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2022-11-08 14:27:42 +0530

    Update doublylink.c

--- doublylink.c
+++ doublylink.c
@@ -3,126 +3,134 @@
 #include<stdlib.h>
 struct abc
 {
-    int pr;
-    char name[20];
-    struct abc *prv,*next;
+  int pr;
+  char name[20];
+  struct abc *prv, *next;
 };
-void insertS(struct abc*h)
+void insertS (struct abc *h)
 {
-    struct abc* new;
-    new=(struct abc*)malloc(sizeof(struct abc));
-printf("enter the prn and name of secretary\n");
-    scanf("%d %s",&new->pr,new->name);
-    new->prv=h;
-    h->next=new;
-    new->next=NULL;
+  struct abc *new;
+  new = (struct abc *) malloc (sizeof (struct abc));
+  printf ("enter the prn and name of secretary\n");
+  scanf ("%d %s", &new->pr, new->name);
+  new->prv = h;
+  h->next = new;
+  new->next = NULL;
 }
-void insertM(struct abc*head)
-{
-    struct abc* new;
 
-    new=(struct abc*)malloc(sizeof(struct abc));
-    printf("Enter the prn and name");
-    scanf("%d",&new->pr);
-    scanf(" %s",new->name);
-    new->next=head->next;
-    new->prv=head;
-    new->next->prv=new;
-    head->next=new;
-}
-void display1(struct abc*head)
+void insertM (struct abc *head)
 {
-    struct abc* temp;
-    temp=head;
-    while(temp!=NULL)
+  struct abc *new;
+
+  new = (struct abc *) malloc (sizeof (struct abc));
+  printf ("Enter the prn and name");
+  scanf ("%d", &new->pr);
+  scanf (" %s", new->name);
+  new->next = head->next;
+  new->prv = head;
+  new->next->prv = new;
+  head->next = new;
+}
+
+void display1 (struct abc *head)
+{
+  struct abc *temp;
+  temp = head;
+  while (temp != NULL)
     {
-        printf("\nName= %s prn no.= %d \n",temp->name,temp->pr);
-        temp=temp->next;
+      printf ("\nName= %s prn no.= %d \n", temp->name, temp->pr);
+      temp = temp->next;
 
     }
 }
-void deleteS(struct abc*head)
-{
-struct abc*temp;
-temp=head;
-while(temp->next!=NULL)
-{
-temp=temp->next;
-}
-temp->prv->next=NULL;
-free(temp);
 
-}
-struct abc *deleteP(struct abc*head)
+void deleteS (struct abc *head)
 {
-    struct abc*temp;
-    temp=head;
-    if(head->next!=NULL)
-    head->next->prv=NULL;
-    head=head->next;
-    free(temp);
-    return(head);
-}
-int count(struct abc*head)
-{
-    struct abc*temp;
-    temp=head;
-    int g=0;/*if g= then take temp->next!=NULL*/
-    while(temp!=NULL)
+  struct abc *temp;
+  temp = head;
+  while (temp->next != NULL)
     {
-        temp=temp->next;
-        g++;
+      temp = temp->next;
     }
-    return(g);
+  temp->prv->next = NULL;
+  free (temp);
+
 }
-int main() {
-   // Write C code here
-   struct abc *head;
-   head=(struct abc*)malloc(sizeof(struct abc));
-   printf("enter the data of president\n");
-printf("\nEnter the prn and name\n");
-   scanf("%d %s",&head->pr,head->name);
-   head->prv=NULL;
-   head->next=NULL;
-   insertS(head);
-   int choice,g;
-   do
-   {
-       printf("\nEnter your choice\n");
-        printf("1.insert a member\n");
-          printf("2.display all members\n");
-                   //printf("3.count of no. of members");
-                   printf("3.delete secretary");
-                   printf("4.delete president");
-                   printf("5.no. of members");
-       printf("0.exit\n");
-       scanf("%d",&choice);
-       switch(choice)
-       {
 
-           case 1:
-           insertM(head);
-           break;
-           case 2:
-           display1(head);
-           break;
-                    case 3:
-           deleteS(head);
-           break;
-           case 4:
-           head=deleteP(head);
-           break;
-           case 5:
-           g=count(head);
-           printf("%d",g);
-           break;
-           case 0:
-           printf("you have successfully exited\n");
-           break;
-       }
-   }while(choice!=0);
+struct abc * deleteP (struct abc *head)
+{
+  struct abc *temp;
+  temp = head;
+  if (head->next != NULL)
+    head->next->prv = NULL;
+  head = head->next;
+  free (temp);
+  return (head);
+}
+
+int count (struct abc *head)
+{
+  struct abc *temp;
+  temp = head;
+  int g = 0;			/*if g= then take temp->next!=NULL */
+  while (temp != NULL)
+    {
+      temp = temp->next;
+      g++;
+    }
+  return (g);
+}
+
+int main ()
+{
+  // Write C code here
+  struct abc *head;
+  head = (struct abc *) malloc (sizeof (struct abc));
+  printf ("enter the data of president\n");
+  printf ("\nEnter the prn and name\n");
+  scanf ("%d %s", &head->pr, head->name);
+  head->prv = NULL;
+  head->next = NULL;
+  insertS (head);
+  int choice, g;
+  do
+    {
+      printf ("\nEnter your choice\n");
+      printf ("1.insert a member\n");
+      printf ("2.display all members\n");
+      //printf("3.count of no. of members");
+      printf ("3.delete secretary");
+      printf ("4.delete president");
+      printf ("5.no. of members");
+      printf ("0.exit\n");
+      scanf ("%d", &choice);
+      switch (choice)
+	{
+
+	case 1:
+	  insertM (head);
+	  break;
+	case 2:
+	  display1 (head);
+	  break;
+	case 3:
+	  deleteS (head);
+	  break;
+	case 4:
+	  head = deleteP (head);
+	  break;
+	case 5:
+	  g = count (head);
+	  printf ("%d", g);
+	  break;
+	case 0:
+	  printf ("you have successfully exited\n");
+	  break;
+	}
+    }
+  while (choice != 0);
 
 
 
-    return 0;
+  return 0;
 }
