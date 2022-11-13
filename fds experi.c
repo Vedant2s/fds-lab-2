6698ad0826a2b7d241e4b6997bd89173555f1c76
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2022-11-13 17:01:33 +0530

    Update fds experi.c

--- fds experi.c
+++ fds experi.c
@@ -3,39 +3,40 @@ int r,c;
 void transpose()
 {
     int a[10][10], transpose[10][10], r, c;
-  printf("Enter rows and columns: ");
-  scanf("%d %d", &r, &c);
+    int *p;
+    p=&a[0][0];
+  printf("Enter size of array: ");
+  scanf("%d", &r);
 
   // asssigning elements to the matrix
   printf("\nEnter matrix elements:\n");
   for (int i = 0; i < r; ++i)
-  for (int j = 0; j < c; ++j) {
+  for (int j = 0; j < r; ++j) {
     printf("Enter element a%d%d: ", i + 1, j + 1);
-    scanf("%d", &a[i][j]);
+    scanf("%d", (p+((i*r)+j)));
+    transpose[j][i] = *(p+((i*r)+j));
   }
 
   // printing the matrix a[][]
   printf("\nEntered matrix: \n");
-  for (int i = 0; i < r; ++i)
-  for (int j = 0; j < c; ++j) {
-    printf("%d  ", a[i][j]);
-    if (j == c - 1)
-    printf("\n");
+  for (int i = 0; i < r; ++i){
+  for (int j = 0; j < r; ++j) {
+    printf("%d  ", *(p+((i*r)+j)));
+    
+    
+  }
+      printf("\n");
   }
 
-  // computing the transpose
-  for (int i = 0; i < r; ++i)
-  for (int j = 0; j < c; ++j) {
-    transpose[j][i] = a[i][j];
-  }
 
   // printing the transpose
   printf("\nTranspose of the matrix:\n");
-  for (int i = 0; i < c; ++i)
+  for (int i = 0; i < r; ++i){
   for (int j = 0; j < r; ++j) {
     printf("%d  ", transpose[i][j]);
-    if (j == r - 1)
-    printf("\n");
+   
+  }
+      printf("\n");
   }
 }
 void multiply()
@@ -44,43 +45,41 @@ void multiply()
     lop=&a[0][0];
     sof=&b[0][0];
  
-    printf("enter the number of row=");    
+    printf("enter the size of matrix = ");    
     scanf("%d",&r);    
-    printf("enter the number of column=");    
-    scanf("%d",&c);    
     printf("enter the first matrix element=\n");    
     for(i=0;i<r;i++)    
     {    
-    for(j=0;j<c;j++)    
+    for(j=0;j<r;j++)    
     {    
-    scanf("%d",&*(lop + ((i * 3) + j)));    
+    scanf("%d",&*(lop + ((i * r) + j)));    
     }    
     }    
     printf("enter the second matrix element=\n");    
     for(i=0;i<r;i++)    
     {    
-    for(j=0;j<c;j++)    
+    for(j=0;j<r;j++)    
     {    
-    scanf("%d",&*(sof + ((i * 3) + j)));    
+    scanf("%d",&*(sof + ((i * r) + j)));    
     }    
     }    
 
     printf("multiply of the matrix=\n");    
     for(i=0;i<r;i++)    
     {    
-    for(j=0;j<c;j++)    
+    for(j=0;j<r;j++)    
     {    
     mul[i][j]=0;    
-    for(k=0;k<c;k++)    
+    for(k=0;k<r;k++)    
     {    
-    mul[i][j]+=*(lop + ((i * 3) + k))**(sof + ((k * 3) + j));    
+    mul[i][j]+=*(lop + ((i * r) + k))**(sof + ((k * r) + j));    
     }    
-    }    
+    }
     }    
     //for printing result    
     for(i=0;i<r;i++)    
     {    
-    for(j=0;j<c;j++)    
+    for(j=0;j<r;j++)    
     {    
     printf("%d\t",mul[i][j]);    
     }    
@@ -128,24 +127,19 @@ void addition()
 	}
       printf ("\n");
     }
+    printf ("\nthe sum of the two matrix is\n");
   for (i = 0; i < 3; i++)
     {
       for (j = 0; j < 3; j++)
 	{
 	  c[i][j] = *(p + ((i * 3) + j)) + *(q + ((i * 3) + j));
-
+        
+        printf ("%d\t", c[i][j]);
 	}
+	printf ("\n");
     }
-  printf ("\nthe sum of the two matrix is\n");
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-	{
-
-	  printf ("%d\t", c[i][j]);
-	}
-      printf ("\n");
-    }
+  
+  
 }
 void saddlepoint()
 {
@@ -161,7 +155,7 @@ for (i = 0; i < num; i++)
 for (j = 0; j < num; j++)
 {
     printf("enter the a[%d][%d]\t",i,j);
-scanf("%d", (p1 + ((i * 3) + j)));
+scanf("%d", (p1 + ((i * num) + j)));
 }
 }
 printf(" The Entered Matrix is :\n");
@@ -170,28 +164,28 @@ for (i = 0; i < num; i++)
 printf("\n");
 for (j = 0; j < num; j++)
 {
-printf("%d ", *(p1 + ((i * 3) + j)));
+printf("%d ", *(p1 + ((i * num) + j)));
 }
 }
 //Compute Saddle Point
 for (i = 0; i < num; i++)
 {
 p = 0;
-sm = *(p1 + ((i * 3) + 0));
+sm = *(p1 + ((i * num) + 0));
 for (j = 0; j < num; j++)
 {
-if (sm > *(p1 + ((i * 3) + j)))
+if (sm > *(p1 + ((i * num) + j)))
 {
-sm = *(p1 + ((i * 3) + j));
+sm = *(p1 + ((i * num) + j));
 p = j;
 }
 }
 large = 0;
 for ( j = 0; j < num; j++)
 {
-if (large < *(p1 + ((j * 3) + p)))
+if (large < *(p1 + ((j * num) + p)))
 {
-large = *(p1 + ((j * 3) + p));
+large = *(p1 + ((j * num) + p));
 }
 }
 if ( sm == large)
